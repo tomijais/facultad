81a07d6bb63240dca72b48fa027225d3780526d7
Tomas Jais <tjais@emergencias.com.ar>
2021-08-21 18:41:59 -0300

    modulo 2

---
+++ act_modulo_2.py
@@ -0,0 +1,8 @@
+num1 = int(input("Ingrese el primer numero: \n"))
+num2 = int(input("Ingrese el segundo numero: \n"))
+
+
+print("El resultado de la suma entre ambos numeros es de: ",num1 + num2)
+print("El resultado de la diferencia entre ambos numeros es de: ",num1 / num2)
+print("El resultado del producto entre ambos numeros es de: ",num1 * num2)
+print("El resultado de de elevar el primero al segundo es de: ",num1 ** num2  )
