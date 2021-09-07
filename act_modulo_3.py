9843a0699ccf13f75f08bf39593b189d668e3853
Tomas Jais <tjais@emergencias.com.ar>
2021-09-07 19:24:51 -0300

    manera horrible de resolverlo pero no vimos arrays

--- act_modulo_3.py
+++ act_modulo_3.py
@@ -15,24 +15,69 @@
 # El nombre de la ciudad con la mayor cantidad de habitaciones. 
 
 
-contador = 0
+contador = 1
 cantidad_huespedes_total = 0
 nombre_mas_habitaciones = ""
 cantidad_mas_habitaciones = 0
 
-while contador < 10:
+porcentajeHotel1 = 0
+porcentajeHotel2 = 0
+porcentajeHotel3 = 0
+porcentajeHotel4 = 0
+porcentajeHotel5 = 0
+porcentajeHotel6 = 0
+porcentajeHotel7 = 0
+porcentajeHotel8 = 0
+porcentajeHotel9 = 0
+porcentajeHotel10 = 0
+
+while contador <= 1:
     nombre_actual = input("Ingrese el nombre de la ciudad de la filial: ")
     total_actual = int(input("Ingrese la capacidad total del hotel (en cuanto a huéspedes: "))
     habitaciones_actual = int(input("Ingrese la cantidad de habitaciones: "))
-    huespedes_mes_actual = int(input("Ingrese la cantidad de huéspedes en un mes"))
+    huespedes_mes_actual = int(input("Ingrese la cantidad de huéspedes en un mes: "))
 
     cantidad_huespedes_total =+ total_actual
     
+    huespedes_mes_actual*100/total_actual
+
+    if contador == 1:
+        porcentajeHotel1 = huespedes_mes_actual*100/total_actual
+    elif contador == 2:
+        porcentajeHotel2 = huespedes_mes_actual*100/total_actual
+    elif contador == 3:
+        porcentajeHotel4 = huespedes_mes_actual*100/total_actual
+    elif contador == 4:
+        porcentajeHotel4 = huespedes_mes_actual*100/total_actual
+    elif contador == 5:
+        porcentajeHotel5 = huespedes_mes_actual*100/total_actual
+    elif contador == 6:
+        porcentajeHotel6 = huespedes_mes_actual*100/total_actual
+    elif contador == 7:
+        porcentajeHotel7 = huespedes_mes_actual*100/total_actual
+    elif contador == 8:
+        porcentajeHotel8 = huespedes_mes_actual*100/total_actual
+    elif contador == 9:
+        porcentajeHotel9 = huespedes_mes_actual*100/total_actual
+    elif contador == 10:
+        porcentajeHotel10 = huespedes_mes_actual*100/total_actual
+
     if habitaciones_actual > cantidad_mas_habitaciones:
         nombre_mas_habitaciones = nombre_actual
         cantidad_mas_habitaciones = habitaciones_actual
 
     contador = contador + 1
 
-print(f"Cantidad de huéspedes que puede alojar toda la cadena de hoteles: {cantidad_huespedes_total}")
-print(f"El nombre de la ciudad con la mayor cantidad de habitaciones: {nombre_mas_habitaciones} con {cantidad_mas_habitaciones}")
+print("Cantidad de huéspedes que puede alojar toda la cadena de hoteles: {cantidad_huespedes_total}")
+print("El nombre de la ciudad con la mayor cantidad de habitaciones es: {nombre_mas_habitaciones} con {cantidad_mas_habitaciones}")
+
+print("El porcentaje de ocupacion del hotel 1 es de: {porcentajeHotel1}")
+print("El porcentaje de ocupacion del hotel 2 es de: {porcentajeHotel2}")
+print("El porcentaje de ocupacion del hotel 3 es de: {porcentajeHotel3}")
+print("El porcentaje de ocupacion del hotel 4 es de: {porcentajeHotel4}")
+print("El porcentaje de ocupacion del hotel 5 es de: {porcentajeHotel5}")
+print("El porcentaje de ocupacion del hotel 6 es de: {porcentajeHotel6}")
+print("El porcentaje de ocupacion del hotel 7 es de: {porcentajeHotel7}")
+print("El porcentaje de ocupacion del hotel 8 es de: {porcentajeHotel8}")
+print("El porcentaje de ocupacion del hotel 9 es de: {porcentajeHotel9}")
+print("El porcentaje de ocupacion del hotel 10 es de: {porcentajeHotel10}")
