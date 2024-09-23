e3c2cae913646929382e46fd296fedb5d60a3c46
Tomas Jais <tomijais@gmail.com>
2024-09-22 22:24:43 -0600

    feat: class 9

---
+++ declarative_programming/class_9_more_recursividad.hs
@@ -0,0 +1,14 @@
+(^^^) :: Int -> Int -> Int
+(^^^) m 0 = 1
+(^^^) m 1 = m
+(^^^) 0 n = n
+(^^^) 1 n = 1
+(^^^) m n = m * (m (^^^) (n-1))
+
+
+
+euclid :: (Integral a, Ord a) => a -> a -> a
+euclid n m
+    | n == m = n
+    | m > n = euclid(m-n) n
+    | otherwise = euclid m (n-m)
